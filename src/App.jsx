44c985d2e531aa8f0f39560f822a26af6f986ece
AhnEunJin <modic970515@gmail.com>
2025-04-03 05:29:23 +0900

    selecting items

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHand,
+  faHandBackFist,
+  faHandScissors,
+} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import Box from './components/Box';
 
 function App() {
-    return (
-        <>
-            <div className='box-container'>
-                <Box title='YOU' />
-                <Box title='COMPUTER' />
-            </div>
-            <div className='btn-container'>
-                <button></button>
-                <button></button>
-                <button></button>
-            </div>
-        </>
-    );
+  const choice = {
+    rock: {
+      name: 'rock',
+      img: 'https://blackbearwow.github.io/image/rockPaperScissors/rock.png',
+    },
+    scissors: {
+      name: 'scissors',
+      img: 'https://blackbearwow.github.io/image/rockPaperScissors/scissors.png',
+    },
+    paper: {
+      name: 'paper',
+      img: 'https://blackbearwow.github.io/image/rockPaperScissors/paper.png',
+    },
+  };
+
+  const play = (userChoice) => {};
+
+  return (
+    <>
+      <h1 className='title'>Let's Play</h1>
+      <div className='box-container'>
+        <Box title='YOU' />
+        <Box title='COMPUTER' />
+      </div>
+
+      <div className='btn-container'>
+        <button className='scissors' onClick={() => play('scissors')}>
+          <FontAwesomeIcon icon={faHandScissors} />
+        </button>
+        <button className='rock' onClick={() => play('rock')}>
+          <FontAwesomeIcon icon={faHandBackFist} />
+        </button>
+        <button className='paper' onClick={() => play('paper')}>
+          <FontAwesomeIcon icon={faHand} />
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default function Box({ title }) {
-    return (
-        <div className='box'>
-            <h1>{title}</h1>
-            <img
-                className='item-img'
-                src='https://blackbearwow.github.io/image/rockPaperScissors/rock.png'
-                alt=''
-            />
-            <h2>WIN</h2>
-        </div>
-    );
+  return (
+    <div className='box'>
+      <h1>{title}</h1>
+      <img
+        className='item-img'
+        src='https://blackbearwow.github.io/image/rockPaperScissors/rock.png'
+        alt=''
+      />
+      <h2>WIN</h2>
+    </div>
+  );
 }
