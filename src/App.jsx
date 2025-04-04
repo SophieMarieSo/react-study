6eea4a7c3aed2109cf5e0a54f831cd1552c69146
AhnEunJin <modic970515@gmail.com>
2025-04-05 02:13:07 +0900

    current location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+    });
+  };
+
   return <div></div>;
 }
 
