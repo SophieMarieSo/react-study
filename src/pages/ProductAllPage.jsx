af9059eb969731ea657ab0116c743701018dd2fc
EunJin An <modic970515@gmail.com>
2025-04-11 06:17:28 +0900

    loading & error

--- src/pages/ProductAllPage.jsx
+++ src/pages/ProductAllPage.jsx
@@ -1,33 +1,56 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Alert, Col, Container, Row, Spinner } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
 import { useSearchParams } from 'react-router';
 
 export default function ProductAllPage() {
   const [productList, setProductList] = useState([]);
   const [query, setQuery] = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     getProducts();
   }, [query]);
 
   const getProducts = async () => {
-    const searchQuery = query.get('q') || '';
-    const resp = await axios.get(
-      `https://my-json-server.typicode.com/SophieMarieSo/react-study/products?q=${searchQuery}`
-    );
-    setProductList(resp.data);
+    setLoading(true);
+    setError(false);
+
+    try {
+      const searchQuery = query.get('q') || '';
+      const resp = await axios.get(
+        `https://my-json-server.typicode.com/SophieMarieSo/react-study/products?q=${searchQuery}`
+      );
+      setProductList(resp.data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setErrorMsg(error.message);
+    }
   };
 
   return (
     <Container className='all'>
       <Row>
-        {productList.map((product, idx) => (
-          <Col lg={3} md={6} sm={12}>
-            <ProductCard key={idx} product={product} />
-          </Col>
-        ))}
+        {error && (
+          <Alert className='error' variant='danger'>
+            {errorMsg}
+          </Alert>
+        )}
+        {loading ? (
+          <Spinner className='loading' animation='border' variant='secondary' />
+        ) : (
+          <>
+            {productList.map((product, idx) => (
+              <Col lg={3} md={6} sm={12}>
+                <ProductCard key={idx} product={product} />
+              </Col>
+            ))}
+          </>
+        )}
       </Row>
     </Container>
   );
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -4,79 +4,115 @@ import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Badge, Col, Container, Dropdown, Image, Row } from 'react-bootstrap';
+import {
+  Alert,
+  Badge,
+  Col,
+  Container,
+  Dropdown,
+  Image,
+  Row,
+  Spinner,
+} from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 export default function ProductDetailPage() {
   const [product, setProduct] = useState([]);
   const { id } = useParams();
   const [wishList, setWishList] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     getProductDetail();
   }, []);
 
   const getProductDetail = async () => {
-    const resp = await axios.get(
-      `https://my-json-server.typicode.com/SophieMarieSo/react-study/products/${id}`
-    );
-    setProduct(resp.data);
+    setLoading(true);
+    setError(false);
+
+    try {
+      const resp = await axios.get(
+        `https://my-json-server.typicode.com/SophieMarieSo/react-study/products/${id}`
+      );
+      setProduct(resp.data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setErrorMsg(error.message);
+    }
   };
 
   return (
     <Container className='detail'>
       <Row>
-        <Col lg={5} sm={12}>
-          <Image
-            className='detail-img'
-            src={product?.img}
-            alt={`product-detail-img-${product?.id}`}
-            fluid
-            rounded
-          />
-        </Col>
-        <Col lg={7} sm={12} className='mt-3'>
-          <h4 className='title-section'>
-            <div className='title-badge'>
-              <span>{product?.title} </span>
-              {product?.new ? (
-                <Badge className='new' bg='warning' text='dark' pill>
-                  신제품
-                </Badge>
-              ) : (
-                ''
-              )}
-            </div>
-            <div
-              className='wishlist'
-              onClick={() => setWishList((prev) => !prev)}
-            >
-              {wishList ? (
-                <FontAwesomeIcon icon={fullHeart} className='full-heart' />
-              ) : (
-                <FontAwesomeIcon icon={faHeart} />
-              )}
-            </div>
-          </h4>
-          <h6 className='choice'>
-            {product?.choice ? 'Conscious choice' : ''}
-          </h6>
-          <h4 className='price'>₩ {product?.price?.toLocaleString('ko-KR')}</h4>
-          <div className='click-section'>
-            <Dropdown>
-              <Dropdown.Toggle variant='danger'>사이즈 선택</Dropdown.Toggle>
+        {error && (
+          <Alert className='error' variant='danger'>
+            {errorMsg}
+          </Alert>
+        )}
+        {loading ? (
+          <Spinner className='loading' animation='border' variant='secondary' />
+        ) : (
+          <>
+            <Col lg={5} sm={12}>
+              <Image
+                className='detail-img'
+                src={product?.img}
+                alt={`product-detail-img-${product?.id}`}
+                fluid
+                rounded
+              />
+            </Col>
+            <Col lg={7} sm={12} className='mt-3'>
+              <h4 className='title-section'>
+                <div className='title-badge'>
+                  <span>{product?.title} </span>
+                  {product?.new ? (
+                    <Badge className='new' bg='warning' text='dark' pill>
+                      신제품
+                    </Badge>
+                  ) : (
+                    ''
+                  )}
+                </div>
+                <div
+                  className='wishlist'
+                  onClick={() => setWishList((prev) => !prev)}
+                >
+                  {wishList ? (
+                    <FontAwesomeIcon icon={fullHeart} className='full-heart' />
+                  ) : (
+                    <FontAwesomeIcon icon={faHeart} />
+                  )}
+                </div>
+              </h4>
+              <h6 className='choice'>
+                {product?.choice ? 'Conscious choice' : ''}
+              </h6>
+              <h4 className='price'>
+                ₩ {product?.price?.toLocaleString('ko-KR')}
+              </h4>
+              <div className='click-section'>
+                <Dropdown>
+                  <Dropdown.Toggle variant='danger'>
+                    사이즈 선택
+                  </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                {product?.size?.map((s, idx) => (
-                  <Dropdown.Item key={idx}>{s}</Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-            <div className='cart'>
-              <FontAwesomeIcon icon={faCartPlus} />
-            </div>
-          </div>
-        </Col>
+                  <Dropdown.Menu>
+                    {product?.size?.map((s, idx) => (
+                      <Dropdown.Item key={idx}>{s}</Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+                <div className='cart'>
+                  <FontAwesomeIcon icon={faCartPlus} />
+                </div>
+              </div>
+            </Col>
+          </>
+        )}
       </Row>
     </Container>
   );
