2ca6987c6ff17e9780cbbf2ee572c6f8715c7739
EunJin An <modic970515@gmail.com>
2025-04-17 22:27:30 +0900

    무한로딩 해결

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Container, Nav } from 'react-bootstrap';
+import { Container, Nav, Navbar as RNavbar } from 'react-bootstrap';
 
 export default function Navbar() {
   return (
-    <Navbar expand='lg' className='bg-body-tertiary'>
+    <RNavbar expand='lg' className='bg-body-tertiary'>
       <Container fluid>
-        <Navbar.Brand href='#'>코딩알려주는 누나 도서관</Navbar.Brand>
-        <Navbar.Toggle aria-controls='navbarScroll' />
-        <Navbar.Collapse id='navbarScroll'>
+        <RNavbar.Brand href='#'>코딩알려주는 누나 도서관</RNavbar.Brand>
+        <RNavbar.Toggle aria-controls='navbarScroll' />
+        <RNavbar.Collapse id='navbarScroll'>
           <Nav className='me-auto'>
             <Nav.Link href='/'>메인</Nav.Link>
             <Nav.Link href='/favorite'>나의 책</Nav.Link>
             <Nav.Link href='/login'>로그인</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
+        </RNavbar.Collapse>
       </Container>
-    </Navbar>
+    </RNavbar>
   );
 }
