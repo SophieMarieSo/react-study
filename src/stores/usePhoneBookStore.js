ef590c84aa6dfb5091a911321175cee41a76f41b
EunJin An <modic970515@gmail.com>
2025-04-30 04:02:46 +0900

    localstorage에 저장

--- src/stores/usePhoneBookStore.js
+++ src/stores/usePhoneBookStore.js
@@ -1,14 +1,27 @@
 import { create } from 'zustand';
 import { v4 as uuidv4 } from 'uuid';
 
+// 로컬스토리지에서 데이터 불러오기
+const loadFromLocalStorage = () => {
+  const savedPhoneBook = localStorage.getItem('phoneBook');
+  return savedPhoneBook ? JSON.parse(savedPhoneBook) : [];
+};
+
+// 로컬스토리지에 데이터 저장하기
+const saveToLocalStorage = (phoneBook) => {
+  localStorage.setItem('phoneBook', JSON.stringify(phoneBook));
+};
+
 const addContact = (set) => (contact) => {
-  set((state) => ({
-    phoneBook: [...state.phoneBook, { id: uuidv4(), ...contact }],
-  }));
+  set((state) => {
+    const newPhoneBook = [...state.phoneBook, { id: uuidv4(), ...contact }];
+    saveToLocalStorage(newPhoneBook);
+    return { phoneBook: newPhoneBook };
+  });
 };
 
 const usePhoneBookStore = create((set) => ({
-  phoneBook: [],
+  phoneBook: loadFromLocalStorage(),
   addContact: addContact(set),
 }));
 
--- src/stores/useThemeStore.js
+++ src/stores/useThemeStore.js
@@ -1,12 +1,15 @@
 // useThemeStore.js
 import { create } from 'zustand';
 
+const toggleMode = (set) => (mode) => {
+  set((state) => {
+    const newMode = state.mode === 'light' ? 'dark' : 'light';
+    localStorage.setItem('color-mode', newMode);
+    return { mode: newMode };
+  });
+};
+
 export const useThemeStore = create((set) => ({
   mode: localStorage.getItem('color-mode') || 'light',
-  toggleMode: () =>
-    set((state) => {
-      const newMode = state.mode === 'light' ? 'dark' : 'light';
-      localStorage.setItem('color-mode', newMode);
-      return { mode: newMode };
-    }),
+  toggleMode: toggleMode(set),
 }));
